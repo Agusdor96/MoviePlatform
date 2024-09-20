97505ecdbe3a9914f58f6a22ac38a2743da5a508
agusdor96 <miroetto12@gmail.com>
2024-09-20 01:35:14 -0300

    Add imports

--- FrontConReact/src/context/MovieContext.jsx
+++ FrontConReact/src/context/MovieContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useContext } from "react";
+import axios from "axios"
 
 const MovieContext = createContext()
 
--- FrontConReact/src/main.jsx
+++ FrontConReact/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { MovieProvider } from './context/MovieContext.jsx';
 // import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MovieProvider> {/* Envuelve tu App con MovieProvider */}
+      <App />
+    </MovieProvider>
   </StrictMode>,
-)
+);
