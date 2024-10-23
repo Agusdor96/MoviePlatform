17862c411d02a78888775308828e0a86f4066a91
agusdor96 <miroetto12@gmail.com>
2024-10-23 09:44:08 -0300

    ADD cookie-parser

--- back/src/server.js
+++ back/src/server.js
@@ -3,12 +3,13 @@ const indexRouter = require("./routes/indexRouter");
 const morgan = require("morgan");
 const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorHandler");
+const cookieParser = require('cookie-parser');
 
 const app = express();
-
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json()); 
+app.use(cookieParser());
 app.use(indexRouter);
 
 app.use(errorHandler)
