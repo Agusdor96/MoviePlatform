921734c46d663fed21bf3c3473677c5885056570
agusdor96 <miroetto12@gmail.com>
2024-11-16 17:06:08 -0300

    vercel

--- src/config/dbCon.js
+++ src/config/dbCon.js
@@ -1,27 +1,17 @@
 require("dotenv").config();
 const mongoose  = require("mongoose");
 
-
-let isConnected = false; // Variable para mantener la conexión persistente
-
 const dbCon = async () => {
-  if (isConnected) {
-    console.log("Usando conexión existente a MongoDB");
-    return; // Si ya estamos conectados, no reconectamos
-  }
-
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: true,
-    });
-    isConnected = true;
-    console.log("Conexión exitosa a MongoDB");
-  } catch (error) {
-    console.error("Error al conectar con MongoDB:", error);
-    throw new Error("Failed to connect to MongoDB");
-  }
+   try {
+      await mongoose.connect(process.env.MONGO_URI, {
+         autoIndex: true,
+      });
+      console.log("Conexión exitosa a MongoDB");
+   } catch (error) {
+      console.error("Error al conectar con MongoDB:", error);
+      throw new Error("Failed to connect to MongoDB");
+   }
 };
+
 
 module.exports = dbCon;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 const dbCon = require("./config/dbCon");
 const app = require ("./server");
 
-dbCon()
-  .then(() => {
-    const port = process.env.PORT || 3000;
+dbCon().then(() => {
+    const port = process.env.PORT || 3000; // Usar el puerto correcto en producción
     app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+       console.log(`Server is running on port ${port}`);
     });
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database:", error);
-    process.exit(1); // Termina el proceso si hay un error de base de datos
-  });
+ }).catch((err) => {
+    console.error("Error al conectar con la base de datos:", err);
+    process.exit(1); // Salir si la conexión falla
+ });
 
 
