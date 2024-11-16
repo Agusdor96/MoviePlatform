8b765afef8e1ff0ffa178b068de26a16fdec102b
agusdor96 <miroetto12@gmail.com>
2024-11-16 16:57:17 -0300

    vercel

--- src/config/dbCon.js
+++ src/config/dbCon.js
@@ -1,12 +1,27 @@
 require("dotenv").config();
-
 const mongoose  = require("mongoose");
 
+
+let isConnected = false; // Variable para mantener la conexión persistente
+
 const dbCon = async () => {
-   await mongoose.connect(process.env.MONGO_URI,{
-      autoIndex:true
-   });
-   console.log("Connected to MongoDB")
+  if (isConnected) {
+    console.log("Usando conexión existente a MongoDB");
+    return; // Si ya estamos conectados, no reconectamos
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: true,
+    });
+    isConnected = true;
+    console.log("Conexión exitosa a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar con MongoDB:", error);
+    throw new Error("Failed to connect to MongoDB");
+  }
 };
 
 module.exports = dbCon;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const dbCon = require("./config/dbCon");
 const app = require ("./server");
 
-dbCon().then((res) => {
-    const port = process.env.PORT || 3000; // Asigna el puerto de Vercel o usa el puerto 3000 localmente
+dbCon()
+  .then(() => {
+    const port = process.env.PORT || 3000;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
-  
-    })
-})
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1); // Termina el proceso si hay un error de base de datos
+  });
+
 
